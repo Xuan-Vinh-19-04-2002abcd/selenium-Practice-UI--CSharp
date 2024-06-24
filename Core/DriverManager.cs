d10c2e41c5e8a0d581814b523ff31ccf0273a066
xuand <xuandangvinh@gmail.com>
2024-06-24 22:09:24 +0700

    feat: create DriverManager

---
+++ Core/DriverManager.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace Test.Core;
+
+public class DriverManager
+{
+    public static IWebDriver WebDriver ;
+        
+    public static void InitializeBrowser(string browser)
+    {
+     
+        if (browser == "chrome"){
+            WebDriver = new ChromeDriver(ChromeDriverService.CreateDefaultService());
+        }
+        else if (browser == "firefox")
+        {
+            WebDriver = new FirefoxDriver();
+        }
+        
+    }
+
+    public static void CloseDriver()
+    {
+        WebDriver.Dispose();
+        WebDriver.Quit();
+        
+    }
+}
